1c5ab9b4c269120c1d22595597f38c446f35f318
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2012-09-09 10:42:46 -0400

    Vidé la config de mail
    Signed-off-by: Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>

--- config/mail.php
+++ config/mail.php
@@ -5,8 +5,8 @@ return array(
         'from' => '', // Send mails from this address.
         'from_name' => '',
         'async' => FALSE, // If async is enabled, emails are not sent immediately but appended to a queue.
-        'queue_path' => 'mailqueue',
-        'salt' => 'asdasfafj239r8290qut5gq',
+        'queue_path' => APPPATH . 'mailqueue',
+        'salt' => NULL,
     ),
 );
 ?>
