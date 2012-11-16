1f420c97d0c7dfc13f4edbad8a8b6a00702c3b43
Paul-Willy Jean <paul1993willy@gmail.com>
2012-11-16 13:49:16 -0500

    plain body
    - Le formattage est géré dans le tableau

--- views/mail/layout/template.php
+++ views/mail/layout/template.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') or die('No direct script access.'); ?><!DOCTYPE html>
 <html>
     <head><?php echo $head ?></head>
-    <body style="margin-left: 30px; font-family: Arial;">
+    <body>
         <?php echo $header ?>
         <?php echo $content ?>
         <?php echo $footer ?>
