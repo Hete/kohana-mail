8797c025c166aab2ae3196970ce6b2948de8a3e0
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2014-08-04 14:11:07 -0400

    Skip tests with RECEIVER is not defined.

--- tests/MailTest.php
+++ tests/MailTest.php
@@ -15,52 +15,42 @@ class MailTest extends Unittest_TestCase {
 	/**
 	 * Set a custom email to receive the test results.
 	 */
-	const RECEIVER = 'foo@example.com';
+	const RECEIVER = NULL;
+
+	public function setUp()
+	{
+		parent::setUp();
+		
+		if (MailTest::RECEIVER === NULL)
+		{
+			$this->markTestSkipped('You have to set a receiver in order to get the test results.');
+		}
+	}
 
 	public function providerSender()
 	{
-		return array(array(Mail_Sender::factory('Mail', array())), 
-			array(Mail_Sender::factory('PEAR_Mail', array())), 
-			array(Mail_Sender::factory('PEAR_Sendmail', array())), 
-			array(Mail_Sender::factory('PEAR_SMTP', array())), 
-			array(Mail_Sender::factory('PHPMailer_Mail', array())), 
-			array(Mail_Sender::factory('PHPMailer_Qmail', array())), 
-			array(Mail_Sender::factory('PHPMailer_Sendmail', array())), 
-			array(Mail_Sender::factory('PHPMailer_SMTP', array())), 
-			array(Mail_Sender::factory('Mock', array())));
+		return array(array('Mail', array()), array('PEAR_Mail', array()), 
+			array('PEAR_Sendmail', array()), array('PEAR_SMTP', array()), 
+			array('PHPMailer_Mail', array()), array('PHPMailer_Qmail', array()), 
+			array('PHPMailer_Sendmail', array()), array('PHPMailer_SMTP', array()), 
+			array('Mock', array()));
 	}
 
 	/**
 	 * @dataProvider providerSender
 	 */
-	public function testSend(Mail_Sender $sender)
+	public function testSend($name, array $options)
 	{
-		$this->assertTrue($sender->subject('test')
+		$this->assertTrue(Mail_Sender::factory($name, $options)->subject('test')
+			->from(($name))
 			->body('test')
-			->send('foo@example.com'));
+			->send(MailTest::RECEIVER));
 	}
 
 	public function testSendWithDefaultSender()
 	{
 		$this->assertTrue(Mailer::factory()->subject('test')
-			->body('test')
-			->send('foo@example.com'));
-	}
-
-	/**
-	 * @dataProvider providerSender
-	 */
-	public function testSubject(Mail_Sender $sender)
-	{
-		$this->assertTrue($sender->subject('Hello Foo')
-			->body('test')
-			->send(MailTest::RECEIVER));
-		
-		$this->assertTrue($sender->subject('¤ Hello Foo ¤')
-			->body('test')
-			->send(MailTest::RECEIVER));
-		
-		$this->assertTrue($sender->subject('')
+			->from(get_class(Mailer::factory()))
 			->body('test')
 			->send(MailTest::RECEIVER));
 	}
@@ -68,17 +58,46 @@ class MailTest extends Unittest_TestCase {
 	/**
 	 * @dataProvider providerSender
 	 */
-	public function testHeaders(Mail_Sender $sender)
+	public function testSubject($name, array $options)
 	{
-		$this->assertTrue($sender->subject('test')
+		// ascii subect
+		$this->assertTrue(Mail_Sender::factory($name, $options)->subject('Hello Foo')
+			->from($name)
+			->body('test')
+			->send(MailTest::RECEIVER));
+		
+		// non-ascii subject
+		$this->assertTrue(Mail_Sender::factory($name, $options)->subject('¤ Hello Foo ¤')
+			->from($name)
+			->body('test')
+			->send(MailTest::RECEIVER));
+		
+		// empty subject
+		$this->assertTrue(Mail_Sender::factory($name, $options)->subject('')
+			->from($name)
+			->body('test')
+			->send(MailTest::RECEIVER));
+	}
+
+	/**
+	 * @dataProvider providerSender
+	 */
+	public function testHeaders($name, array $options)
+	{
+		$this->assertTrue(Mail_Sender::factory($name, $options)->subject('test')
+			->from($name)
 			->body('test')
 			->headers('Content-Type', 'text/html')
 			->send(MailTest::RECEIVER));
 	}
 
-	public function testParam()
+	/**
+	 * @dataProvider providerSender
+	 */
+	public function testParam($name, array $options)
 	{
-		$this->assertTrue(Mailer::factory()->subject('Mail sent by :name')
+		$this->assertTrue(Mail_Sender::factory($name, $options)->subject('Mail sent by :name')
+			->from($name)
 			->body('Hi, it\'s :name, how are you?')
 			->param(':name', 'Foo')
 			->send(MailTest::RECEIVER));
@@ -89,16 +108,18 @@ class MailTest extends Unittest_TestCase {
 	/**
 	 * @dataProvider providerSender
 	 */
-	public function testBody(Mail_Sender $sender)
+	public function testBody($name, array $options)
 	{
 		// html body
-		$this->assertTrue($sender->subject('Hey foo!')
+		$this->assertTrue(Mail_Sender::factory($name, $options)->subject('Hey foo!')
+			->from($name)
 			->content_type('text/html')
 			->body('<html><body>Hey foo!</body></html>')
 			->send(MailTest::RECEIVER));
 		
 		// plain text body
-		$this->assertTrue($sender->subject('Hey foo!')
+		$this->assertTrue(Mail_Sender::factory($name, $options)->subject('Hey foo!')
+			->from($name)
 			->body('Hey!')
 			->send(MailTest::RECEIVER));
 	}
@@ -106,9 +127,10 @@ class MailTest extends Unittest_TestCase {
 	/**
 	 * @dataProvider providerSender
 	 */
-	public function testAttachment(Mail_Sender $sender)
+	public function testAttachment($name, array $options)
 	{
-		$this->assertTrue($sender->subject('Sent you some files!')
+		$this->assertTrue(Mail_Sender::factory($name, $options)->subject('Sent you some files!')
+			->from($name)
 			->body('Hey!')
 			->attachment('{}', array('Content-Type' => 'application/json'))
 			->attachment(file_get_contents(MODPATH . 'mail/tests/test.png'), array(
@@ -116,7 +138,8 @@ class MailTest extends Unittest_TestCase {
 			->send(MailTest::RECEIVER));
 		
 		// with filename
-		$this->assertTrue($sender->subject('Sent you some files!')
+		$this->assertTrue(Mail_Sender::factory($name, $options)->subject('Sent you some files!')
+			->from($name)
 			->body('Hey!')
 			->attachment('{}', array('Content-Type' => 'application/json'))
 			->attachment(file_get_contents(MODPATH . 'mail/tests/test.png'), array(
