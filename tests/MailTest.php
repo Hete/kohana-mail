0a31c2347813179687c45761479029bdfcafa138
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2014-12-03 17:05:05 -0500

    Disable tests for PEAR as well, it's not available on Travis CI.

--- tests/MailTest.php
+++ tests/MailTest.php
@@ -49,9 +49,9 @@ class MailTest extends Unittest_TestCase {
 	{
 		return array(
 			array(Mail_Sender::factory('Mail',               array(), array())),
-			array(Mail_Sender::factory('PEAR_Mail',          array(), array())),
-			array(Mail_Sender::factory('PEAR_Sendmail',      array(), array())),
-			array(Mail_Sender::factory('PEAR_SMTP',          array(), array())),
+			//array(Mail_Sender::factory('PEAR_Mail',          array(), array())),
+			//array(Mail_Sender::factory('PEAR_Sendmail',      array(), array())),
+			//array(Mail_Sender::factory('PEAR_SMTP',          array(), array())),
 			//array(Mail_Sender::factory('PHPMailer_Mail',     array(), array())),
 			//array(Mail_Sender::factory('PHPMailer_Qmail',    array(), array())), // could not test this one :(
 			// array(Mail_Sender::factory('PHPMailer_Sendmail', array(), array())),
